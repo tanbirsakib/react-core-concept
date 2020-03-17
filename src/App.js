b3af897520105adb626fc12c89046c2d0b0b9d91
tanbirsakib <you@example.com>
2020-03-17 23:05:11 +0600

    added html in js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var makeup = {
+    backgroundColor :'gray',
+    color : 'cyan'
+  }
+  var sakib = {name:"Muhammed Tanbir Hossen Sakib",
+               dream:"A passionate webdeveloper",
+               Dept:"CSE"
+}
+  var youmon = {name:"Emon Younes",
+  dream:"Databse developer",
+  Dept:"CSE"
+
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,13 +23,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h5 style={makeup}>My info : {sakib.name + " " +"Dream:" + sakib.dream}</h5>
+        <h5 style = {makeup}>info : {youmon.name + " " + youmon.Dept}</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React with react
         </a>
       </header>
     </div>
